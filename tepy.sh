2afb99653519e02e4032d85e42afb25f1b9726df
ivgtr <gear.1112t.o@gmail.com>
2021-01-07 14:10:45 +0900

    :sparkles: add: ass a error hundling.

--- tepy.sh
+++ tepy.sh
@@ -3,12 +3,26 @@
 tepy () {
   template_path="$HOME/.tepy/template"
   template_list=()
+  if [ ! -d $template_path ];then
+    echo "Not found tempate directory."
+    mkdir $template_path
+    return
+  fi
+
   for template in `find $template_path -maxdepth 1 -mindepth 1 -type d`; do
     template_list+=${template##*/}
   done
 
-  select i in $template_list; do
-    cp -i -r -v `find "${template_path}/${i}" -maxdepth 1 -mindepth 1` "$pwd"
-    break
-  done
+  if [ ${#template_list[@]} -gt 0 ]; then
+    select i in $template_list; do
+      cp -i -r -v `find "${template_path}/${i}" -maxdepth 1 -mindepth 1` "$pwd"
+      break
+    done
+    echo "completed."
+  else
+    echo "Not found tempate. pls add a directory."
+    open $template_path
+  fi
+
+  return
 }
