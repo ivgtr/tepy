34e1a3718633c3e885592708164dd3c912dcb003
ivgtr <gear.1112t.o@gmail.com>
2021-01-07 13:52:41 +0900

    :sparkles: add: add a main function

--- tepy.sh
+++ tepy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 tepy () {
-  if [ "$2" != "" ]; then
-    echo $2
-  fi
+  template_path="$HOME/.tepy/template"
+  template_list=()
+  for template in `find $template_path -maxdepth 1 -mindepth 1 -type d`; do
+    template_list+=${template##*/}
+  done
+
+  select i in $template_list; do
+    cp -i -r -v `find "${template_path}/${i}" -maxdepth 1 -mindepth 1` "$pwd"
+    break
+  done
 }
